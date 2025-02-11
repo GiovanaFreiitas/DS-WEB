7c4ed90daff39ed1f6867b092f58da28c34310aa
GiovanaFreiitas <giovanafreitas9870@gmail.com>
2025-02-11 08:47:53 -0300

    Atividade-01

---
+++ poo/Atividade-01/exemplo.php
@@ -0,0 +1,26 @@
+<?php
+
+class veiculo{
+    
+    //Atributos:
+    public $marca;
+
+    protected $seguro;
+
+    private $renavam;
+
+    //Metodo:
+    public function ligar(){
+        return "o meu nome é".
+    }
+
+}
+
+//Instanciando meus objetos
+
+$polo = new Veliculo();
+$polo-> marca = "Wolkswagem";
+$povo-> renavam = "123123123";
+
+
+?>
